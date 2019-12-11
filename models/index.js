c8ab32c74351d0b0e84d0e5fd8ec3d6e7bd70190
pcreem <creempepper@gmail.com>
2019-12-12 02:52:29 +0800

    pgLize

--- models/index.js
+++ models/index.js
@@ -5,15 +5,15 @@ const path = require('path')
 const Sequelize = require('sequelize')
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development'
-const config = require(__dirname + '/../config/config.json')[env]
+const config = require(__dirname + '/../config/config.js')[env]
 const db = {}
 
 // 資料庫連線
 let sequelize
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config)
+if (config.url) {
+  sequelize = new Sequelize(config.url, config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config)
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 // 動態引入其他 models
